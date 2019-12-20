5042bd4b522e0e5f88a2a81d451a9c2fd95a5062
Hikarukiku <hikarukiku@gmail.com>
2019-12-20 18:55:42 +0300

    v2

--- template_method.py
+++ template_method.py
@@ -3,42 +3,42 @@ from abc import ABC, abstractmethod
 
 
 class AbstractUser(ABC):
-    def template_registration(self) -> None:
+    def template_registration(self):
         self.get_login()
         self.get_email()
         self.get_id()
         self.get_password()
         self.send_data()
 
-    def get_login(self) -> None:
+    def get_login(self):
         print("Login is suitable!")
 
-    def get_email(self) -> None:
+    def get_email(self):
         print("Email is suitable!")
 
-    def get_password(self) -> None:
+    def get_password(self):
         print("Password is suitable!")
 
     @abstractmethod
-    def get_id(self) -> None:
+    def get_id(self):
         pass
 
     @abstractmethod
-    def send_data(self) -> None:
+    def send_data(self):
         pass
 
 class GeneralUser(AbstractUser):
-    def get_id(self) -> None:
+    def get_id(self):
         pass
 
-    def send_data(self) -> None:
+    def send_data(self):
         print("Data sent to users table!")
 
 class Authority(AbstractUser):
-    def get_id(self) -> None:
+    def get_id(self):
         print("Your ID is confirmed!")
 
-    def send_data(self) -> None:
+    def send_data(self):
         print("Data sent to authority table!")
 
 def do_registration(user: AbstractUser):
